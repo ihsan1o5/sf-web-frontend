0ffcc146830248d54c2fdecf4d611878276739c6
Ihsanullah Siddique <ihsan@elltechnologies.com>
2026-01-19 08:07:24 +0500

    new changes

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 // EC2 API Url
-export const API_URL = "https://api.oxipay.digital/api";
+// export const API_URL = "https://api.oxipay.digital/api";
 
 // Local API Url
-// export const API_URL = "http://localhost:3000/api";
+export const API_URL = "http://localhost:3000/api";
 
 export const CLOUDINARY_UPLOAD_URL = "https://api.cloudinary.com/v1_1";
